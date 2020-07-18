50ae0afda2c59ca37ccc05e8776a6f24ea0bce82
martinDM <hello@martindm.uk>
2020-07-18 18:45:19 +0100

    Adds build

---
+++ build/precache-manifest.dcc822350b9d0885a8d991e81729f9a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "271369dc63e2e51ff21026af4903e0d7",
+    "url": "/static/media/bg1.271369dc.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d69edfd5c304cbe4234f",
+    "url": "/static/js/main.d69edfd5.chunk.js"
+  },
+  {
+    "revision": "89519119e8b46f4d7291",
+    "url": "/static/js/1.89519119.chunk.js"
+  },
+  {
+    "revision": "d69edfd5c304cbe4234f",
+    "url": "/static/css/main.9dcb0b2b.chunk.css"
+  },
+  {
+    "revision": "89519119e8b46f4d7291",
+    "url": "/static/css/1.8c8ff18b.chunk.css"
+  },
+  {
+    "revision": "3cbe216c05e21f1107635a208e8f5c2c",
+    "url": "/index.html"
+  }
+];
